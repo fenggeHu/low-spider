2ae09cc36391f71e8f596827ee8fef2c2225ada3
max.hu <pbfoxhu@gmail.com>
2024-01-29 16:33:37 +0800

    修改htmltable兼容性

--- src/main/java/spider/handler/HtmlTableHandler.java
+++ src/main/java/spider/handler/HtmlTableHandler.java
@@ -18,7 +18,7 @@ import java.util.List;
  **/
 @Slf4j
 public class HtmlTableHandler implements Handler {
-    // 通常表头只有1行，路径指定到cell - "#oMainTable > thead > tr > th"
+    // 通常表头只有1行，路径指定到cell - "#oMainTable > thead > tr"
     private String theadSelector;
     // 路径到行 - "#oMainTable > tbody > tr"
     private String tbodyTrSelector;
@@ -33,8 +33,9 @@ public class HtmlTableHandler implements Handler {
         Document doc = Jsoup.parse(context.getBody());
         String[] header = null;
         if (null != this.theadSelector) {   // 有些header不标准，不处理
-            Elements head = doc.select(theadSelector); // 选择到 thead->tr->th
-            if (null != head && !head.isEmpty()) {
+            Elements thead = doc.select(theadSelector); // 选择到 thead->tr
+            if (null != thead && thead.size() == 1) {
+                Elements head = thead.get(0).select("td,th");   // 有些非标准的故意混淆
                 header = new String[head.size()];
                 for (int i = 0; i < head.size(); i++) { // th
                     header[i] = head.get(i).text();
@@ -42,12 +43,12 @@ public class HtmlTableHandler implements Handler {
             }
         }
 
-        Elements main = doc.select(tbodyTrSelector); // 选择到 tbody->tr
+        Elements tbody = doc.select(tbodyTrSelector); // 选择到 tbody->tr
         List<String[]> ret = new LinkedList<>();
-        if (null != main) {
-            for (Element e : main) {  // 遍历tr
-                Elements tds = e.getElementsByTag("td");
-                if (tds.size() <= 0) continue;    // 忽略空行或非数据行
+        if (null != tbody) {
+            for (Element e : tbody) {  // 遍历tr
+                Elements tds = e.select("td,th");   // 有些非标准的故意混淆
+                if (tds.size() == 0) continue;    // 忽略空行或非数据行
                 String[] values = new String[tds.size()];
                 for (int i = 0; i < tds.size(); i++) {
                     values[i] = tds.get(i).text();
