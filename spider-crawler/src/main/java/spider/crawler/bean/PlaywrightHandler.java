7e50f6fe2af5faff716dee5f96c5e0632ce70fdd
max.hu <pbfoxhu@gmail.com>
2024-12-24 18:27:48 +0800

    for test

--- spider-crawler/src/main/java/spider/crawler/bean/PlaywrightHandler.java
+++ spider-crawler/src/main/java/spider/crawler/bean/PlaywrightHandler.java
@@ -42,7 +42,7 @@ public class PlaywrightHandler {
 
     private void playwright() {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
             page.navigate(url);
